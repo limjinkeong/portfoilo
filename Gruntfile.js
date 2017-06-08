dae9612d33211675ba278ad5f368bd62a06c765c
limjinkeong <limjinkeong33@gmail.com>
2017-06-08 16:05:16 +0900

    GRUNT LESS SETTING

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,52 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    //서버
-    serve: {
-        options: {
-            port: 9000
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //서버
+        serve: {
+            options: {
+                port: 9000
+            }
+        },
+        //레스
+        less: {
+            product: {
+                options: {
+                    paths: ['assets/css'],
+                    compress:true
+                },
+                files: {
+                    'assets/css/style.min.css': 'assets/less/import.less'
+                }
+            },
+            dev: {
+                options: {
+                    paths: ['assets/css'],
+                    dumpLineNumbers:"comments"
+                },
+                files: {
+                    'assets/css/style.dev.css': 'assets/less/import.less'
+                }
+            }
+        },
+        //와치
+        watch: {
+            scripts: {
+                files: ['assets/less/*.less'],
+                tasks: ['less'],
+                options: {
+                    spawn: false,
+                },
+            },
         }
-    },
-    //레스
-    //와치
-  });
+    });
 
-  // Load the plugin that provides the "uglify" task.
-grunt.loadNpmTasks('grunt-serve');
-
-  // Default task(s).
-  grunt.registerTask('default', ['serve']);
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-serve');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Default task(s).
+    grunt.registerTask('default', ['serve']);
 
 };
